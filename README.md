3276d26460b36387f3741420908d955ac16ace46
kingkoala <bajprodo@gmail.com>
2023-04-13 19:57:57 +0900

    Boiler Plate Done

--- README.md
+++ README.md
@@ -3,10 +3,24 @@ To be used on Yamaha work later on..
 
 ## Resources
 
+[Sample Test Login](https://practicetestautomation.com/practice-test-login/)
+
 [Webdriver Manager Repo](https://github.com/SergeyPirogov/webdriver_manager)
+
+[Selenium Locators](https://www.selenium.dev/documentation/webdriver/elements/locators/)
+
+
 
 ## Cheat Sheet
 
+xpath format = `//tag[@attribute='value']`
+
 | Command | Description |
 |---|---|
-| test() | *test* |
+| `driver.get()` | Opens up link in browser |
+| `driver.find_element(By.locator, "locator str")` | Returns locator element |
+| `driver.current_url` | Returns the current url of the browser |
+| `locator.text` | Returns text attr of a locator |
+| `locator.click()` | Clicks a locator, mostly used on buttons |
+| `locator.send_keys(str)` | Types in the string argument in a locators field |
+| `locator.is_displayed()` | Return whether the locator is visible to the user |
--- main.py
+++ main.py
@@ -1,18 +1,59 @@
+"""
+Open page
+Type username student into Username field
+Type password Password123 into Password field
+Puch Submit button
+Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+Verify button Log out is displayed on the new page
+"""
+
+URL = "https://practicetestautomation.com/practice-test-login/"
+USERNAME = "student"
+PASSWORD = "Password123"
+
+
 ## Open Browser
 
 # selenium 4 and google chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager # type: ignore
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-time.sleep(5)
 
 ## Go to webpage
-URL = "https://practicetestautomation.com/practice-test-login/"
+
 driver.get(URL)
+time.sleep(2)
 
 
 
-# 
+## Type username student into Username field
+username_locator = driver.find_element(By.ID, "username")
+username_locator.send_keys(USERNAME)
+
+## Type password Password123 into Password field
+password_locator = driver.find_element(By.ID, "password")
+password_locator.send_keys(PASSWORD)
+
+## Push Submit button
+submit_btn_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+submit_btn_locator.click()
+time.sleep(2)
+
+## Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+post_url = driver.current_url
+assert "practicetestautomation.com/logged-in-successfully/" in post_url
+
+
+## Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+expected_text_list = ['Congratulations', 'Logged In Successfully']
+result_text_locator = driver.find_element(By.XPATH, "//h1[@class='post-title']")
+assert any(map(result_text_locator.text.__contains__, expected_text_list))
+
+## Verify button Log out is displayed on the new page
+logout_btn_locator = driver.find_element(By.XPATH, "//a[@class='wp-block-button__link has-text-color has-background has-very-dark-gray-background-color']")
+assert logout_btn_locator.is_displayed()
